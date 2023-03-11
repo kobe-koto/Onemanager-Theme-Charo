d65b59291e8628a6c179f010ff30aec806e8af2c
kobe-koto <91669733+kobe-koto@users.noreply.github.com>
2023-03-11 22:19:52 +0800

    [Done] Path Bar & File/Folder List

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import vue from "@astrojs/vue";
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  site: "https://aom.koto.gq/",
+  build: {
+    format: "file",
+    assets: "aom-assets/_astro"
+  },
+
   integrations: [vue()]
 });
